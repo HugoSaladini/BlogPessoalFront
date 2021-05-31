fb98cc6bb09a8ecf325cdd38778bd76b7b04b04b
HugoSaladini <hg.saladini@gmail.com>
2021-05-31 13:54:19 -0300

    criado consumo de API Login e Cadastro

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { from } from 'rxjs';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   {path: '' , redirectTo: 'entrar' , pathMatch: 'full'},
 
   {path: 'entrar' , component: EntrarComponent},
-  {path: 'cadastrar' , component: CadastrarComponent}
+  {path: 'cadastrar' , component: CadastrarComponent},
+
+  {path: 'inicio' , component: InicioComponent}
 
 ]
 
