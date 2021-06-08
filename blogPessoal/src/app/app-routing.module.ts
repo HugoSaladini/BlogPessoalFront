9f5fe3c693356f156bef4fdb4ad29f15a34b1ed6
HugoSaladini <hg.saladini@gmail.com>
2021-06-08 10:08:12 -0300

    Add PUT DEL e TEMA

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,20 +1,28 @@
 import { EntrarComponent } from './entrar/entrar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { from } from 'rxjs';
+
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 
 const routes: Routes = [
+
   {path: '' , redirectTo: 'entrar' , pathMatch: 'full'},
 
   {path: 'entrar' , component: EntrarComponent},
+  
   {path: 'cadastrar' , component: CadastrarComponent},
 
   {path: 'inicio' , component: InicioComponent},
 
-  {path: 'tema' , component: TemaComponent}
+  {path: 'tema' , component: TemaComponent},
+
+  {path: 'tema-edit/:id' , component: TemaEditComponent},
+
+  {path: 'tema-delete/:' , component: TemaDeleteComponent}
 
 ]
 
